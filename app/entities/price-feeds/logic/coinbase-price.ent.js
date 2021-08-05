aad26295748c282c5f596999cf073af6a596c5a9
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-05 21:13:52 +0300

    scaffold coinbase price fetching

---
+++ app/entities/price-feeds/logic/coinbase-price.ent.js
@@ -0,0 +1,23 @@
+/**
+ * @fileoverview Fetches price of coibase.
+ */
+
+const axios = require('axios');
+
+const entity = (module.exports = {});
+
+entity.API_COINBASE_BASE = 'https://api.coinbase.com/v2/prices/';
+
+/**
+ * Get the price of coinbase.
+ *
+ * @param {string=} currencyPair The currency pair to fetch price for.
+ * @return {Promise<string>} A Promise.
+ */
+entity.getPriceCoinbase = async (currencyPair = 'BTC-USD') => {
+  const url = `${entity.API_COINBASE_BASE}${currencyPair}/spot`;
+
+  const res = await axios.get(url);
+
+  return res?.data?.data?.amount;
+};
--- app/entities/price-feeds/price-feeds.ent.js
+++ app/entities/price-feeds/price-feeds.ent.js
@@ -3,9 +3,14 @@
  */
 
 const priceFeedService = require('./price-feed.service');
+
+const { getPriceCoinbase } = require('./logic/coinbase-price.ent');
+
 const log = require('../../services/log.service').get();
 
 const entity = (module.exports = {});
+
+entity.getPriceCoinbase = getPriceCoinbase;
 
 /**
  * Initialize the entity and service.
