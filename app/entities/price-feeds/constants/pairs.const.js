c0e85a7d8fe0e5caccd777c98e47d66324cd1093
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-07 17:57:13 +0300

    create the local, normalized pairs

---
+++ app/entities/price-feeds/constants/pairs.const.js
@@ -0,0 +1,14 @@
+/**
+ * @fileoverview Internal normalized pairs.
+ */
+
+const consts = (module.exports = {});
+
+/** @const {Object<string>} PAIRS Internal normalized pairs. */
+consts.PAIRS = {
+  BTCUSD: 'BTCUSD',
+  ETHUSD: 'ETHUSD',
+  LINKUSD: 'LINKUSD',
+  UNIUSD: 'UNIUSD',
+  AAVEUSD: 'AAVEUSD',
+};
