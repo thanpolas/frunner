2b75cbf5dfefe25d0b807db6ea17d6f4b7ab05ba
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-07 19:56:27 +0300

    create prices assertion

---
+++ test/assert/prices.assert.js
@@ -0,0 +1,59 @@
+/**
+ * @fileoverview Asserts prices value from all dexes / cexes.
+ */
+
+const assert = (module.exports = {});
+
+/**
+ * Determines if the provided object is of type that complies with the API Spec.
+ *
+ * @param {Object} testObj The object to test.
+ * @throws {Error} if assertions failed.
+ */
+assert.assert = (testObj) => {
+  if (!testObj) {
+    throw new Error('Empty object passed on assertion');
+  }
+
+  assert.assertProperties(testObj);
+  assert.assertTypes(testObj);
+  assert.assertValues(testObj);
+};
+
+/**
+ * Assert the test object has the expected properties.
+ *
+ * @param {Object} testObj The object to test.
+ * @throws {Error} if assertions failed.
+ */
+assert.assertProperties = (testObj) => {
+  expect(testObj).toContainAllKeys([
+    'BTCUSD',
+    'ETHUSD',
+    'LINKUSD',
+    'UNIUSD',
+    'AAVEUSD',
+  ]);
+};
+
+/**
+ * Assert the test object's properties have the expected types.
+ *
+ * @param {Object} testObj The object to test.
+ * @throws {Error} if assertions failed.
+ */
+assert.assertTypes = (testObj) => {
+  expect(testObj.BTCUSD).toBeString();
+  expect(testObj.ETHUSD).toBeString();
+  expect(testObj.LINKUSD).toBeString();
+  expect(testObj.UNIUSD).toBeString();
+  expect(testObj.AAVEUSD).toBeString();
+};
+
+/**
+ * Assert the test object's properties have the expected values.
+ *
+ * @param {Object} testObj The object to test.
+ * @throws {Error} if assertions failed.
+ */
+assert.assertValues = (/* testObj */) => {};
