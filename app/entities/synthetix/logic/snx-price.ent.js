09bf3f546d789dcd10cb10d2d8d1898ce696bf31
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-07 18:29:28 +0300

    remove clutter from snx entity

--- app/entities/synthetix/logic/snx-price.ent.js
+++ app/entities/synthetix/logic/snx-price.ent.js
@@ -15,13 +15,7 @@ entity.snxPrice = async () => {
   const snxjs = getClient();
   const { formatEther } = snxjs.utils;
 
-  const synths = snxjs.synths.map(({ name }) => name);
-  console.log('synths', synths);
-  // const fromBlock = 10260987;
-  // const blockOptions = fromBlock ? { blockTag: Number(fromBlock) } : {};
   const blockOptions = {};
-
-  let totalInUSD = 0;
 
   const unformattedSnxPrice =
     await snxjs.contracts.ExchangeRates.rateForCurrency(
@@ -29,7 +23,6 @@ entity.snxPrice = async () => {
       blockOptions,
     ); // note blockOptions must be passed to `ethers.Contract` as the final parameter (and fails if no archive node)
   const snxPrice = formatEther(unformattedSnxPrice);
-  console.log('snxPrice', snxPrice);
 
   return snxPrice;
 };
