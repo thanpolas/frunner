e1ad44251c530f6f38e7e7dfa2f3c159a14135bd
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-07 22:15:01 +0300

    implement snx prices, returns normalized prices

--- app/entities/synthetix/logic/snx-price.ent.js
+++ app/entities/synthetix/logic/snx-price.ent.js
@@ -3,26 +3,81 @@
  */
 
 const { getClient } = require('../synthetix.service');
+const { PAIRS } = require('../../price-feeds');
 
 const entity = (module.exports = {});
+
+/** @const {Array<string>} SNX_TOKENS SNX tokens we care about */
+entity.SNX_TOKENS = ['sBTC', 'sETH', 'sLINK', 'sUNI', 'sAAVE'];
+
+/** @const {Object<string>} NORMALIZED_PAIRS Normalized pairs */
+entity.NORMALIZED_PAIRS = {
+  sBTC: PAIRS.BTCUSD,
+  sETH: PAIRS.ETHUSD,
+  sLINK: PAIRS.LINKUSD,
+  sUNI: PAIRS.UNIUSD,
+  sAAVE: PAIRS.AAVEUSD,
+};
+
+/** @type {Array<Object>} Prepopulate all pairs as byte32 type on initialization */
+entity.PAIRS_TO_BYTE32 = [];
+
+/**
+ * Warm up the caches.
+ *
+ */
+entity.init = () => {
+  const snxjs = getClient();
+  entity.PAIRS_TO_BYTE32 = entity.SNX_TOKENS.map((token) =>
+    snxjs.toBytes32(token),
+  );
+};
 
 /**
  * Fetches prices for SNX synths.
  *
  * @return {Promise<void>} A Promise.
  */
-entity.snxPrice = async () => {
+entity.snxPrices = async () => {
   const snxjs = getClient();
-  const { formatEther } = snxjs.utils;
-
   const blockOptions = {};
 
-  const unformattedSnxPrice =
-    await snxjs.contracts.ExchangeRates.rateForCurrency(
-      snxjs.toBytes32('SNX'),
+  const promises = entity.PAIRS_TO_BYTE32.map((byte32Token) => {
+    return snxjs.contracts.ExchangeRates.rateForCurrency(
+      byte32Token,
       blockOptions,
-    ); // note blockOptions must be passed to `ethers.Contract` as the final parameter (and fails if no archive node)
-  const snxPrice = formatEther(unformattedSnxPrice);
+    );
+  });
 
-  return snxPrice;
+  const allRes = await Promise.all(promises);
+
+  const snxPrices = entity._parseResults(allRes, snxjs);
+
+  return snxPrices;
+};
+
+/**
+ * Will validate, parse and normalize SNX synth price results.
+ *
+ * @param {Array<string>} allRes On chain price results for SNX synths.
+ * @param {Object} snxjs DI SNX client.
+ * @return {Object<string>} Normalized price object.
+ * @private
+ */
+entity._parseResults = (allRes, snxjs) => {
+  // check for errors
+  if (!allRes.length) {
+    return;
+  }
+
+  const { formatEther } = snxjs.utils;
+
+  const prices = {};
+  allRes.forEach((unformattedSnxPrice, index) => {
+    const snxToken = entity.SNX_TOKENS[index];
+    const normalizedPair = entity.NORMALIZED_PAIRS[snxToken];
+    const snxPrice = formatEther(unformattedSnxPrice);
+    prices[normalizedPair] = snxPrice;
+  });
+  return prices;
 };
