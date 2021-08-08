6c1eca7c0e3c01abc3ec593cab9f1bf7ac9673d5
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-08 22:46:58 +0300

    scaffold link query

--- app/entities/chainlink/logic/link-query-price.ent.js
+++ app/entities/chainlink/logic/link-query-price.ent.js
@@ -4,21 +4,46 @@
 
 const { ethers } = require('ethers');
 
+const { getProvider, network } = require('../../ether');
+const { asyncMapCap } = require('../../../utils/helpers');
+
 const ABI_AGGREGATOR = require('../abi/chain-aggregator.abi.json');
-const { getProvider } = require('../../ether');
+const { ETH_ORACLES_AR } = require('../constants/oracles.const');
 
 const entity = (module.exports = {});
+
+/** @const {number} EXP_DECIMALS Exponent of decimals for calculations */
+const EXP_DECIMALS = 1e8;
+
+/**
+ * Fetches all oracle prices.
+ *
+ * @return {Promise<Object>} The response.
+ */
+entity.getAllPricesChainlink = async () => {
+  const provider = getProvider(network.optimistic_kovan);
+
+  const rawResults = await asyncMapCap(ETH_ORACLES_AR, async (oracle) => {
+    const result = await entity.queryPrice(oracle.address, provider);
+    return {
+      result,
+      decimals: oracle.decimals,
+      name: oracle.name,
+    };
+  });
+
+  const results = entity._processResults(rawResults);
+  return results;
+};
 
 /**
  * Will query the Chainlink oracle for the price.
  *
  * @param {string} oracleAddress The address of the oracle.
- * @param {Object} network Local network object.
- * @return {Promise<>} A Promise.
+ * @param {Object} provider Provider to use.
+ * @return {Promise<Object>} A Promise with the results.
  */
-entity.queryPrice = async (oracleAddress, network) => {
-  const provider = getProvider(network);
-
+entity.queryPrice = async (oracleAddress, provider) => {
   const priceFeedContract = new ethers.Contract(
     oracleAddress,
     ABI_AGGREGATOR,
@@ -38,4 +63,20 @@ entity.queryPrice = async (oracleAddress, network) => {
   // startedAt: BigNumber { _hex: '0x610c0a90', _isBigNumber: true },
   // updatedAt: BigNumber { _hex: '0x610c0a90', _isBigNumber: true },
   // answeredInRound: BigNumber { _hex: '0x020000000000002211', _isBigNumber: true }
+};
+
+/**
+ * Process and normalize the results.
+ *
+ * @param {Array<Object>} rawResults Raw results from the oracle contract.
+ * @return {Object<string>} Normalized object with pairs as keys and prices as
+ *   values.
+ */
+entity._processResults = (rawResults) => {
+  const res = rawResults.map((rawRes) => {
+    const priceRaw = Number(rawRes.result.answer);
+    rawRes.price = priceRaw / EXP_DECIMALS;
+    return rawRes;
+  });
+  return res;
 };
