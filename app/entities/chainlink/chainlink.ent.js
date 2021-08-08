0e9a2f0dd6d6a1f13bbd13c5c19e43756262408d
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-08 22:47:12 +0300

    wire everything up on chainlink entity

--- app/entities/chainlink/chainlink.ent.js
+++ app/entities/chainlink/chainlink.ent.js
@@ -2,8 +2,12 @@
  * @fileoverview Chainlink related functionality and logic.
  */
 
-const { queryPrice } = require('./logic/link-query-price.ent');
+const {
+  queryPrice,
+  getAllPricesChainlink,
+} = require('./logic/link-query-price.ent');
 
-const entity = module.exports = {};
+const entity = (module.exports = {});
 
 entity.queryPrice = queryPrice;
+entity.getAllPricesChainlink = getAllPricesChainlink;
