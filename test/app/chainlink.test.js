300bf9b63fb7e078bc4ad9a516ddae14b4a3a6fe
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-09 13:36:35 +0300

    finalize chainlink oracle test

--- test/app/chainlink.test.js
+++ test/app/chainlink.test.js
@@ -4,9 +4,6 @@
 
 const testLib = require('../lib/test.lib');
 
-// eslint-disable-next-line import/order
-const config = require('config');
-
 const { assert: pricesAssert } = require('../assert/prices.assert');
 
 const { getAllPricesChainlink } = require('../../app/entities/chainlink');
@@ -15,26 +12,10 @@ describe('Fetch Prices from Chainlink Oracles', () => {
   testLib.init();
 
   describe(`Happy Path`, () => {
-    beforeEach(() => {
-      // give an adequate timeout
-      config.app.fetch_price_timeout = 3000;
-    });
     test('Will successfully fetch all expected prices', async () => {
-      const prices = await getAllPricesChainlink();
-      console.log('prices:', prices);
-      pricesAssert(prices);
-    });
-  });
-  describe(`Error Cases`, () => {
-    beforeEach(() => {
-      // give an adequate timeout
-      config.app.fetch_price_timeout = 3000;
-    });
-    test('Will handle timeout gracefully', async () => {
-      // set timeout to 1ms so the request times out.
-      config.app.fetch_price_timeout = 1;
-      const prices = await getAllPricesChainlink();
-      expect(prices).toBeUndefined();
+      const oraclePrices = await getAllPricesChainlink();
+      expect(oraclePrices).toContainAllKeys(['oracleData', 'oracleByPair']);
+      pricesAssert(oraclePrices.oracleByPair);
     });
   });
 });
