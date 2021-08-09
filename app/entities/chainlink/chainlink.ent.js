b1ae1e37f5f8c0874b4e5b858e20c26b896b3f64
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-09 18:05:27 +0300

    export ETH_ORACLES

--- app/entities/chainlink/chainlink.ent.js
+++ app/entities/chainlink/chainlink.ent.js
@@ -7,7 +7,10 @@ const {
   getAllPricesChainlink,
 } = require('./logic/link-query-price.ent');
 
+const { ETH_ORACLES } = require('./constants/oracles.const');
+
 const entity = (module.exports = {});
 
 entity.queryPrice = queryPrice;
 entity.getAllPricesChainlink = getAllPricesChainlink;
+entity.ETH_ORACLES = ETH_ORACLES;
