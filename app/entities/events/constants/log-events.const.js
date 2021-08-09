a1fc57e025037ea6a2421e241bc74295b4f5ed73
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-09 22:19:58 +0300

    create log events

---
+++ app/entities/events/constants/log-events.const.js
@@ -0,0 +1,11 @@
+/**
+ * @fileoverview Enumerate the log events.
+ */
+
+const consts = (module.exports = {});
+
+consts.LogEvents = {
+  DECISION_ENDED: 'decisionEnd',
+  STAYING_COURSE: 'stayingCourse',
+  CUTTING_LOSSES: 'cuttingLosses',
+};
