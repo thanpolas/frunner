e20f4f1c9f855ec5c382eb84e2925ed416b5254b
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-09 21:32:37 +0300

    implement flatFilter() helper

--- app/utils/helpers.js
+++ app/utils/helpers.js
@@ -3,6 +3,7 @@
  */
 
 const Bluebird = require('bluebird');
+const _ = require('lodash');
 
 const helpers = (module.exports = {});
 
@@ -124,4 +125,14 @@ helpers.getDivergence = (a, b) => {
  */
 helpers.wait = (msWait = 1000) => {
   return new Promise((resolve) => setTimeout(resolve, msWait));
+};
+
+/**
+ * Will deep flatten the given array and filter our falsy values.
+ *
+ * @param {Array<*>} ar Array with items.
+ * @return {Array<*>} Flattened and filtered array.
+ */
+helpers.flatFilter = (ar) => {
+  return _.flattenDeep(ar).filter((v) => !!v);
 };
