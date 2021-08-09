a7bfa3ce6a296ed1abfc49f2b91a58113f7a7e0b
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-09 16:36:43 +0300

    create first migration and setup db configs

--- migrations/migrations/20210226111833_first-migration.js
+++ migrations/migrations/20210226111833_first-migration.js
@@ -1,8 +1,36 @@
-// const { defaultFields } = require('../migration-helpers');
+const { defaultFields } = require('../migration-helpers');
 
 exports.up = async function (knex) {
   // Required if gen_random_uuid() PG method doesn't work:
   await knex.schema.raw('CREATE EXTENSION IF NOT EXISTS pgcrypto');
+
+  //
+  // The trades table
+  //
+  await knex.schema.createTable('trades', function (table) {
+    table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
+    table.string('pair');
+    table.float('feed_price').notNullable();
+    table.float('oracle_price').notNullable();
+    table.string('network', 20).notNullable();
+    table.boolean('executed').defaultTo(false).notNullable();
+    table.string('execution_tx');
+    table.float('execution_tokens_total');
+    table.string('execution_token_symbol', 5);
+    table.boolean('closed_trade').defaultTo(false).notNullable();
+    table.timestamp('closed_at');
+    table.string('closed_tx');
+    table.float('closed_profit_loss_number');
+    table.string('closed_profit_loss_percent', 10);
+    table.float('closed_feed_price');
+    table.float('closed_oracle_price');
+    table.boolean('testing').defaultTo(false).notNullable();
+
+    defaultFields(table, knex);
+
+    table.index('pair');
+    table.index('closed_trade');
+  });
 };
 
 exports.down = async function () {
