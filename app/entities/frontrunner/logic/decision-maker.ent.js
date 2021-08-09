df358c7e44a1bf8418b33b8b943041e0d7adebb9
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-09 18:06:10 +0300

    work on frontrunner decision maker

--- app/entities/frontrunner/logic/decision-maker.ent.js
+++ app/entities/frontrunner/logic/decision-maker.ent.js
@@ -2,9 +2,31 @@
  * @fileoverview Decision maker.
  */
 
+const config = require('config');
+
+const { PAIRS_AR } = require('../../price-feeds');
+const { ETH_ORACLES } = require('../../chainlink');
+const {
+  create: tradeCreate,
+  update: tradeUpdate,
+  getById: tradeGetById,
+} = require('../sql/trades.sql');
+const { asyncMapCap, wait } = require('../../../utils/helpers');
+
 const log = require('../../../services/log.service').get();
 
 const entity = (module.exports = {});
+
+/** @type {Object} local copy of the last state pushed from events-plexer */
+entity.lastDivergences = {};
+
+/** @type {Object} local state with active (open) trades */
+entity.activeTrades = {
+  // BTCUSD: {
+  //  id: [...]
+  //  [...] Rest of "trades" model fields
+  // }
+};
 
 /**
  * Will determine if an action needs to be taken based on the divergences
@@ -15,7 +37,23 @@ const entity = (module.exports = {});
  * @private
  */
 entity.determineAction = async (divergences) => {
-  await entity._logHumanReadable(divergences);
+  const lastBlock = entity.lastDivergences?.state?.blockNumber;
+
+  entity.lastDivergences = divergences;
+
+  await Promise.all([
+    entity._newOpportunities(divergences),
+    entity._checkCloseTrades(lastBlock, divergences),
+  ]);
+
+  const opportunities = entity._findOpportunities(divergences);
+
+  // Filter out opportunities that open trades already exist for.
+  const newOpportunities = opportunities.filter(
+    (opportunity) => !entity.activeTrades[opportunity.pair],
+  );
+
+  await asyncMapCap(newOpportunities, entity._executeOpportunity);
 };
 
 /**
@@ -53,3 +91,116 @@ entity._convertToHumarReadable = (singleSetDivergence) => {
 
   return hr;
 };
+
+/**
+ * Discover and execute new trading opportunities.
+ *
+ * @param {Object} divergences The calculated divergences.
+ * @return {Promise<void>} An empty promise.
+ * @private
+ */
+entity._newOpportunities = async (divergences) => {
+  const opportunities = entity._findOpportunities(divergences);
+
+  // Filter out opportunities that open trades already exist for.
+  const newOpportunities = opportunities.filter(
+    (opportunity) => !entity.activeTrades[opportunity.pair],
+  );
+
+  const tradeRecords = await asyncMapCap(
+    newOpportunities,
+    entity._executeOpportunity,
+  );
+};
+
+/**
+ * Will determine if there are trading opportunities based on the oracle
+ * divergence threshold of each token.
+ *
+ * @param {Object} divergences The calculated divergences.
+ * @return {Array<Object>} Opportunities objects.
+ * @private
+ */
+entity._findOpportunities = async (divergences) => {
+  const { oracleToFeed } = divergences;
+
+  const opportunities = [];
+  PAIRS_AR.forEach((pair) => {
+    const divergence = oracleToFeed[pair];
+    const oracle = ETH_ORACLES[pair];
+
+    if (divergence < oracle.deviation) {
+      return;
+    }
+
+    // There is a trading opportunity, mark it
+    const opportunity = {
+      pair,
+      divergence,
+      blockNumber: divergences.state.blockNumber,
+      oraclePrice: divergences.state.oraclePrices[pair],
+      feedPrice: divergences.state.feedPrices[pair],
+    };
+
+    opportunities.push(opportunity);
+  });
+
+  return opportunities;
+};
+
+/**
+ * Execute the opportunity.
+ *
+ * @param {Object} opportunity Local opportunity object.
+ * @return {Promise<Object>} A Promise with the created trade record.
+ * @private
+ */
+entity._executeOpportunity = async (opportunity) => {
+  const { pair } = opportunity;
+
+  // Lock pair on active trades to avoid race conditions
+  entity.activeTrades[pair] = {};
+
+  const tradeData = {
+    pair,
+    feed_price: opportunity.feedPrice,
+    oracle_price: opportunity.oraclePrice,
+    opportunity_blockNumber: opportunity.blockNumber,
+    network: config.app.network,
+    testing: config.app.testing,
+  };
+
+  const tradeId = await tradeCreate(tradeData);
+  let tradeRecord = await tradeGetById(tradeId);
+  entity.activeTrades[opportunity.pair] = tradeRecord;
+
+  if (config.app.testing) {
+    // On testing, emulate trade TX, as if it takes 2s.
+    await wait(2000);
+  } else {
+    // Actually execute the trade.
+  }
+
+  const { state: currentState } = entity.lastDivergences;
+  const traded_feed_price = currentState.feedPrices[pair];
+  const traded_oracle_price = currentState.oraclePrices[pair];
+
+  const tradeUpdateData = {
+    traded: true,
+    traded_feed_price,
+    traded_oracle_price,
+    traded_block_number: currentState.blockNumber,
+    traded_tx: '0x',
+    traded_tokens_total: 1000,
+    traded_token_symbol: 'sUSD',
+  };
+
+  await tradeUpdate(tradeUpdateData);
+
+  tradeRecord = await tradeGetById(tradeId);
+  entity.activeTrades[opportunity.pair] = tradeRecord;
+
+  return tradeRecord;
+};
+
+entity._checkCloseTrades = async (lastBlock, divergences) => {};
