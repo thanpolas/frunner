362ec045cf4798cc89172f9ae25512b4cfb90321
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-12 18:25:38 +0300

    add more explicit assertions on the trade close test

--- test/app/frontrunner-decision-maker.test.js
+++ test/app/frontrunner-decision-maker.test.js
@@ -98,6 +98,10 @@ describe('Frontrunner - Decision Making', () => {
       const [closedTrade] = resultClose.closedTrades;
 
       expect(closedTrade.closed_profit_loss_percent).toEqual('3.00%');
+      expect(closedTrade.closed_price_diff).toEqual(1379.3);
+      expect(Number(closedTrade.closed_profit_loss).toFixed(2)).toEqual(
+        '30.00',
+      );
       tradesAssert(closedTrade, 'BTCUSD', divergencesOpen, divergencesClose);
     });
   });
