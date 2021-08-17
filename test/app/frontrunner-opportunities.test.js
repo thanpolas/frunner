46a5fd261f0b26b1638dd1c857fd7318d9f66c74
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-17 22:20:57 +0300

    create test case for sorting opportunities

---
+++ test/app/frontrunner-opportunities.test.js
@@ -0,0 +1,24 @@
+/**
+ * @fileoverview Test frontrunner opportunities entity.
+ */
+
+const testLib = require('../lib/test.lib');
+
+const { allOpportunities } = require('../fixtures/opportunities.fix');
+
+const {
+  _sortOpportunities,
+} = require('../../app/entities/frontrunner/logic/decision-making/opportunities.ent');
+
+describe('Frontrunner - Opportunities', () => {
+  testLib.init();
+
+  describe(`Happy Path`, () => {
+    test('Will sort opportunities correctly', () => {
+      const opportunities = allOpportunities();
+      expect(opportunities[0].pair).toEqual('ETHUSD');
+      _sortOpportunities(opportunities);
+      expect(opportunities[0].pair).toEqual('LINKUSD');
+    });
+  });
+});
