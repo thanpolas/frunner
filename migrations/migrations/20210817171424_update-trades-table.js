851913fe265aa08b7345ce789f782d2336585cbe
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-17 20:59:26 +0300

    more fields added to migration

--- migrations/migrations/20210817171424_update-trades-table.js
+++ migrations/migrations/20210817171424_update-trades-table.js
@@ -4,7 +4,15 @@ exports.up = async function (knex) {
     table.renameColumn('traded_token_symbol', 'traded_source_token_symbol');
 
     table.float('traded_dst_tokens');
-    table.string('traded_dst_token_symbol');
+    table.string('traded_dst_token_symbol', 10);
+    table.number('traded_gas_spent');
+
+    table.boolean('closed_cut_losses');
+    table.float('closed_source_tokens');
+    table.string('closed_source_token_symbol', 10);
+    table.float('closed_dst_tokens');
+    table.string('closed_dst_token_symbol', 10);
+    table.number('closed_gas_spent');
   });
 };
 
