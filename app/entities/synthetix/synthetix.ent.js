ada5674ff32a51662479c90ff9cea34402a34896
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-17 12:56:43 +0300

    export functions on synthetix entity

--- app/entities/synthetix/synthetix.ent.js
+++ app/entities/synthetix/synthetix.ent.js
@@ -6,10 +6,14 @@ const log = require('../../services/log.service').get();
 
 const { init: initService } = require('./synthetix.service');
 const { init: initPrices, snxPrices } = require('./logic/snx-price.ent');
+const { init: initTrade, snxTrade } = require('./logic/snx-trade.ent');
+const { init: initBalances } = require('./logic/snx-balances.ent');
+// const { init: initApprove } = require('./logic/snx-approve.ent');
 
 const entity = (module.exports = {});
 
 entity.snxPrices = snxPrices;
+entity.snxTrade = snxTrade;
 
 /**
  * Initialize synthetix service.
@@ -20,4 +24,8 @@ entity.init = async () => {
   await log.info('Initializing synthetix...');
   await initService();
   initPrices();
+  await initBalances();
+  await initTrade();
+
+  // await initApprove();
 };
