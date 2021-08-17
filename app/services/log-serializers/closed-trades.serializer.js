c4e60bb3c9d84007fafd8dbbef4423efb12d4f68
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-17 23:15:01 +0300

    fix opened and closed trade log serializers

--- app/services/log-serializers/closed-trades.serializer.js
+++ app/services/log-serializers/closed-trades.serializer.js
@@ -4,8 +4,12 @@
  * @param {string=} usePath Set to override default log write path.
  * @return {function} Serializer for logality.
  */
-module.exports = (usePath = 'context.closedTrades') => {
+module.exports = (usePath = 'context.closedTrade') => {
   return (val) => {
+    if (!val) {
+      return { path: usePath };
+    }
+
     const value = {
       raw: val,
       pair: val.pair,
@@ -16,8 +20,11 @@ module.exports = (usePath = 'context.closedTrades') => {
       traded_source_tokens: val.traded_source_tokens,
       traded_source_token_symbol: val.traded_source_token_symbol,
       closed_tx: val.closed_tx,
-      closed_profit_loss_number: val.closed_profit_loss_number,
-      closed_profit_loss_percent: val.closed_profit_loss_percent,
+      closed_profit_loss: val.closed_profit_loss,
+      traded_projected_percent_hr: val.traded_projected_percent_hr,
+      closed_profit_loss_percent_hr: val.closed_profit_loss_percent_hr,
+      closed_cut_losses: val.closed_cut_losses,
+      testing: val.testing,
     };
 
     return {
--- app/services/log-serializers/opened-trades.serializer.js
+++ app/services/log-serializers/opened-trades.serializer.js
@@ -4,8 +4,11 @@
  * @param {string=} usePath Set to override default log write path.
  * @return {function} Serializer for logality.
  */
-module.exports = (usePath = 'context.openedTrades') => {
+module.exports = (usePath = 'context.openedTrade') => {
   return (val) => {
+    if (!val) {
+      return { path: usePath };
+    }
     const value = {
       raw: val,
 
@@ -16,6 +19,7 @@ module.exports = (usePath = 'context.openedTrades') => {
       traded_tx: val.traded_tx,
       traded_source_tokens: val.traded_source_tokens,
       traded_source_token_symbol: val.traded_source_token_symbol,
+      traded_projected_percent_hr: val.traded_projected_percent_hr,
       testing: val.testing,
     };
 
