b0a190295ea35b73962fcf5607bda9af4c89d1ea
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-31 14:41:29 +0300

    scaffold the command controller on frontrunner entity

---
+++ app/entities/frontrunner/logic/command-controller.ent.js
@@ -0,0 +1,13 @@
+/**
+ * @fileoverview Handles live commands from bot.
+ */
+
+const entity = (module.exports = {});
+
+/**
+ * Start trading if stopped.
+ *
+ * @param {DiscordMessage} message The discord message.
+ * @return {Promise<void>} A Promise.
+ */
+entity.startTrade = async (message) => {};
