b31783ef004ce2d51ead66ec63768e840333feac
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-31 14:42:16 +0300

    refactor and prepare message router for approved member commands

--- app/entities/message-router/logic/router-member-command.ent.js
+++ app/entities/message-router/logic/router-member-command.ent.js
@@ -2,7 +2,10 @@
  * @fileoverview Handles Member commands.
  */
 
-const messages = require('../messages');
+const { msgError, help } = require('../messages');
+const { startTrade } = require('../../frontrunner');
+
+const log = require('../../../services/log.service').get();
 
 const router = (module.exports = {});
 
@@ -13,15 +16,58 @@ const router = (module.exports = {});
  * @return {Promise<void>} A Promise.
  * @private
  */
-router.handleMemberCommands = async (message) => {
-  const [command /* , cmdArgument */] = message.content.split(' ');
+router.handleChannelMessage = async (message) => {
+  const [command] = message.content.split(' ');
 
-  switch (command) {
-    case '!help':
-      await message.channel.send(messages.help());
+  const commandLowerCase = command.toLowerCase();
+  switch (commandLowerCase) {
+    case 'help':
+      await message.channel.send(help());
       break;
+    case 'start':
+      await router._invokeCommand(startTrade, message, 'start', false);
+      break;
+
     default:
-      await message.channel.send(messages.error());
+      await log.warn('handleChannelMessage() :: Bogus command invoked', {
+        custom: {
+          command,
+        },
+        relay: true,
+        commandLowerCase,
+      });
       break;
+  }
+};
+
+/**
+ * Invoke the command with try catch statements.
+ *
+ * @param {function} command The command to invoke.
+ * @param {DiscordMessag} message The message that requested the command.
+ * @param {string} commandName The command name for error logging.
+ * @param {boolean} showTyping Set to true to have the bot appear as typing
+ *    while the operation executes.
+ * @return {Promise<void>} A Promise.
+ * @private
+ */
+router._invokeCommand = async (
+  command,
+  message,
+  commandName,
+  showTyping = false,
+) => {
+  try {
+    if (showTyping) {
+      await message.channel.sendTyping();
+    }
+
+    await command(message);
+  } catch (ex) {
+    await log.error(`_invokeCommand() :: Error on ${commandName}()`, {
+      error: ex,
+      relay: true,
+    });
+    await message.channel.send(msgError());
   }
 };
--- app/entities/message-router/message-router.ent.js
+++ app/entities/message-router/message-router.ent.js
@@ -2,11 +2,22 @@
  * @fileoverview Handle message commands to the bot.
  */
 
+const config = require('config');
+
 const { getClient } = require('../../services/discord.service');
 const { handleMemberCommands } = require('./logic/router-member-command.ent');
 const log = require('../../services/log.service').get();
 
 const messageRouter = (module.exports = {});
+
+/**
+ * @const {Array<strimg>} ALLOWED_MEMBERS Discord member ids of members allowed
+ *    to interact with the bot.
+ */
+const ALLOWED_MEMBERS = config.allowed_members.split(',');
+
+/** @const {Array<strimg>} PUBLIC_COMMANDS Public commands this bot listens to */
+messageRouter.PUBLIC_COMMANDS = ['stop', 'start', 'test', 'help'];
 
 /**
  * Initialize Discord event listeners for performing message router.
@@ -26,8 +37,8 @@ messageRouter.init = () => {
  * @private
  */
 messageRouter._onMessage = async (message) => {
-  // only care for private messages. (public are  type === "text").
-  if (message.channel.type !== 'dm') {
+  // only care for channel messages. (private are  type === "dm").
+  if (message.channel.type !== 'text') {
     return;
   }
 
@@ -36,6 +47,17 @@ messageRouter._onMessage = async (message) => {
 
   // ignore own messages
   if (discordAuthor.id === client.user.id) {
+    return;
+  }
+
+  // only allow approved members
+  if (!ALLOWED_MEMBERS.includes(discordAuthor.id)) {
+    return;
+  }
+
+  // Only care for commands this bot listens to.
+  const [command] = message.content.split(' ');
+  if (!messageRouter.PUBLIC_COMMANDS.includes(command)) {
     return;
   }
 
