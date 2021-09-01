10f5e6af46ec1791fe9672cac35d6e9a3dc8ecca
Thanos Polychronakis <thanpolas@gmail.com>
2021-09-01 15:53:59 +0300

    scaffold the trading roam event handler

---
+++ app/entities/frontrunner/strategies/roam/event-handler.ent.js
@@ -0,0 +1,99 @@
+/**
+ * @fileoverview Handles events and local runtime state of roam trading.
+ */
+
+const { findOpportunity } = require('./trade-roam.ent');
+const { LogEvents } = require('../../../events');
+const { getOpenTrades } = require('../../sql/trades-roam.sql');
+
+const log = require('../../../../services/log.service').get();
+
+const { DECISION_ENDED } = LogEvents;
+
+const entity = (module.exports = {});
+
+/** @type {Object} local state with the active opportunity */
+entity.activeOpportunity = null;
+
+/** @type {boolean} Toggle to ensure determineAction will not have duplicate runs */
+entity._decisionRunning = false;
+
+/**
+ * Warm up the local state from DB.
+ *
+ * @return {Promise<void>} An empty promise.
+ */
+entity.init = async () => {
+  await log.info('Initializing trading roam event handler...');
+  const openTrades = await getOpenTrades();
+
+  if (openTrades.length === 0) {
+    return;
+  }
+
+  if (openTrades.length !== 1) {
+    throw new Error(
+      `More than 1 (${openTrades.length} open roam-trades found)`,
+    );
+  }
+
+  [entity.activeOpportunity] = openTrades;
+};
+
+/**
+ * Will determine if an action needs to be taken based on the divergencies
+ * and the currently stored trading state.
+ *
+ * @param {Object} divergencies The calculated divergencies.
+ * @return {Promise<Object>} A Promise with two array keys: "openedTrades" and
+ *    "closedTrades".
+ * @private
+ */
+entity.determineActionRoam = async (divergencies) => {
+  const result = {
+    openedTrade: null,
+    closedTrade: null,
+  };
+
+  if (entity._decisionRunning) {
+    return result;
+  }
+  entity._decisionRunning = true;
+
+  const [openedTrade, closedTrade] = await Promise.all([
+    findOpportunity(divergencies, entity.activeOpportunity),
+    closeTrade(divergencies, entity.activeOpportunity),
+  ]);
+
+  result.openedTrade = openedTrade;
+  result.closedTrade = closedTrade;
+
+  entity._decisionRunning = false;
+
+  await entity.logResults(divergencies, result);
+
+  return result;
+};
+
+/**
+ * Will log the divergencies in a human readable format.
+ *
+ * @param {Object} divergencies The calculated divergencies.
+ * @param {Object} result The decision making result.
+ * @return {Promise<void>}
+ * @private
+ */
+entity.logResults = async (divergencies, result) => {
+  const { openedTrade, closedTrade } = result;
+
+  if (!openedTrade && !closedTrade) {
+    return;
+  }
+
+  await log.info('Decision Making Ended', {
+    openedTrade,
+    closedTrade,
+    divergencies,
+    relay: DECISION_ENDED,
+  });
+};
