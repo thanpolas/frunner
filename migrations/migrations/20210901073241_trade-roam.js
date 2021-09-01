7439b22404ef75e03c4de57477d8f4c34fe8d2e6
Thanos Polychronakis <thanpolas@gmail.com>
2021-09-01 12:50:31 +0300

    tweak and fix the new migration for trade-roam

--- migrations/migrations/20210901073241_trade-roam.js
+++ migrations/migrations/20210901073241_trade-roam.js
@@ -6,10 +6,9 @@ exports.up = async function (knex) {
   //
   await knex.schema.createTable('trades-roam', function (table) {
     table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
-    table.string('pair');
     table.string('network', 20).notNullable();
 
-    table.string('opportunity_source_token_symbol', 5).notNullable();
+    table.string('opportunity_source_symbol', 5).notNullable();
     table.float('opportunity_source_feed_price').notNullable();
     table.float('opportunity_source_oracle_price').notNullable();
     table.float('opportunity_source_usd_diff_percent').notNullable();
@@ -26,29 +25,29 @@ exports.up = async function (knex) {
 
     table.integer('opportunity_block_number').notNullable();
 
-    table.string('traded_tx').notNullable();
-    table.integer('traded_block_number').notNullable();
+    table.string('traded_tx');
+    table.integer('traded_block_number');
 
-    table.float('traded_source_tokens').notNullable();
-    table.float('traded_dst_tokens').notNullable();
-    table.float('traded_actual_price_between_tokens').notNullable();
+    table.float('traded_source_tokens');
+    table.float('traded_dst_tokens');
+    table.float('traded_actual_ratio_between_tokens');
 
-    table.bigInteger('traded_gas_spent').notNullable();
+    table.bigInteger('traded_gas_spent');
 
     // calculate ratio between source & target using oracle prices and
     // then based on that price, calculate the percentage difference between them.
-    table.float('traded_source_target_diff_percent').notNullable();
-    table.string('traded_source_target_diff_percent_hr', 10).notNullable();
+    table.float('traded_source_target_diff_percent');
+    table.string('traded_source_target_diff_percent_hr', 10);
 
     // Translate the difference into USD
     table.float('traded_profit_loss_usd');
 
-    table.boolean('testing').defaultTo(false).notNullable();
+    table.boolean('testing').defaultTo(false);
 
     defaultFields(table, knex);
 
-    table.index('pair');
-    table.index('closed_trade');
+    table.index('opportunity_source_symbol');
+    table.index('opportunity_target_symbol');
   });
 };
 
