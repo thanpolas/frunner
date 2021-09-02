32131b040ff048f4229a61907c5dff129cc6b7fc
Thanos Polychronakis <thanpolas@gmail.com>
2021-09-02 17:27:44 +0300

    update log-relay unit test

--- test/unit/trade-roam-log-serializer.test.js
+++ test/unit/trade-roam-log-serializer.test.js
@@ -22,12 +22,20 @@ describe('UNIT trade-roam log-relay serialzier', () => {
     const message = _formatRoamTrade(logContext);
 
     expect(message).toEqual(
-      'Closed Roam Trade | **Network**: optimistic_kovan | **Testing**: true' +
-        ' | **Traded**: 10000sUNI -> 10000sLINK | **op %**: -0.05% - 3.00% =' +
-        ' 3.05% | **cl %**: -10.77% | **op feed**: 28.39 - 25.26 | **cl orcl**:' +
-        ' 28.4 - 25.34 | **cl vals**: $284000 - $253400 | **cl pr/ls**: $-30600' +
-        ' | **principal**: 2033.78 (12359.56%) | **op-tr et**: 1" | **tr-cl et' +
-        '**: 1.096"',
+      `--==Closed Roam Trade==--
+**Network**: optimistic_kovan **Testing**: true
+**Traded**: 10000sUNI -> 10000sLINK
+**Opportunity % (source - target = diff)**: -0.05% - 3.00% = 3.05%
+**Close % Profit/Loss**: -10.77%
+**Opportunity Feed Prices**: 28.39 - 25.26
+**Opportunity Oracle Prices**: 28.4 - 24.53
+**Close Oracle Prices**: 28.4 - 25.34
+**Close USD Value (source - target)**: $284000 - $253400
+**Close Profit/Loss**: $-30600
+**Principal and %**: 2033.78 (12359.56%)
+**Opportunity to Trade Elapsed Time (seconds)**: 1"
+**Trade to Close Elapsed Time (seconds)**: 1.096"
+**Trade - Close Block**: 0 - 1054368`,
     );
   });
 });
