3c7ffa96d97853c97d4ec6ca6ab97dc9bc9ac39e
Thanos Polychronakis <thanpolas@gmail.com>
2021-09-03 23:42:58 +0300

    remove ambiguous calculation on trade

--- app/entities/frontrunner/strategies/roam/trade-roam.ent.js
+++ app/entities/frontrunner/strategies/roam/trade-roam.ent.js
@@ -225,7 +225,6 @@ entity._updateTradeRecord = async (tradeRecord, tx) => {
   let traded_source_tokens = 10000;
   let traded_target_tokens = 10000;
   let traded_gas_spent = 0;
-  let traded_actual_ratio_between_tokens = 1;
   let traded_source_usd_value = 10000;
 
   if (tx) {
@@ -236,13 +235,6 @@ entity._updateTradeRecord = async (tradeRecord, tx) => {
       traded_source_tokens * tradeRecord.opportunity_source_oracle_price;
     traded_target_tokens = tx.dstTokenQuantityReadable;
     traded_gas_spent = tx.gasUsed.toString();
-
-    const tokenFraction = [traded_source_tokens, traded_target_tokens];
-    const decimalFraction = [SYNTH_DECIMALS, SYNTH_DECIMALS];
-    traded_actual_ratio_between_tokens = poolTokensToAuto(
-      tokenFraction,
-      decimalFraction,
-    );
   }
 
   const tradeUpdateData = {
@@ -252,7 +244,6 @@ entity._updateTradeRecord = async (tradeRecord, tx) => {
     traded_source_usd_value,
     traded_target_tokens,
     traded_gas_spent,
-    traded_actual_ratio_between_tokens,
     traded_at: db().fn.now(),
   };
 
